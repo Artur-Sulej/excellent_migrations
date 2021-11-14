f4a7f425cf33407a88494b3c31a12b500f223c89
Artur Sulej <mr.artur.su@gmail.com>
2021-11-14 10:24:04 +0100

    Skipping checks listed in config

--- lib/dangers_checker.ex
+++ lib/dangers_checker.ex
@@ -5,6 +5,7 @@ defmodule ExcellentMigrations.DangersChecker do
     ast
     |> Parser.parse()
     |> reject_safety_assured()
+    |> skip_ignored_checks()
   end
 
   defp reject_safety_assured(dangers) do
@@ -13,5 +14,13 @@ defmodule ExcellentMigrations.DangersChecker do
     else
       Keyword.delete(dangers, :safety_assured)
     end
+  end
+
+  defp skip_ignored_checks(dangers) do
+    skipped_types = Application.get_env(:excellent_migrations, :skip_checks, [])
+
+    Enum.reduce(skipped_types, dangers, fn skipped_type, dangers_acc ->
+      Keyword.delete(dangers_acc, skipped_type)
+    end)
   end
 end

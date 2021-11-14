1c8d1cc5a88dfbdbe73576237388f0b3d5c4fc1a
Artur Sulej <mr.artur.su@gmail.com>
2021-11-14 12:12:55 +0100

    Parser rewritten to use Macro.postwalk

--- lib/parser.ex
+++ lib/parser.ex
@@ -1,69 +1,21 @@
 defmodule ExcellentMigrations.Parser do
   def parse(ast) do
-    ast
-    |> _parse([])
+    {_ast, dangers} =
+      Macro.postwalk(ast, [], fn code_part, acc ->
+        new_acc = acc ++ find_dangers(code_part)
+        {code_part, new_acc}
+      end)
+
+    dangers
   end
 
-  defp _parse(
-         {
-           :def,
-           _,
-           [{_name, _, _}, [do: {:__block__, _, body}]]
-         },
-         acc
-       ) do
-    parse_body(body, acc)
+  defp find_dangers(code_part) do
+    find_index_not_concurrently(code_part) ++
+      find_raw_sql(code_part) ++
+      find_safety_assured(code_part)
   end
 
-  defp _parse(
-         {
-           :def,
-           _,
-           [{_name, _, _}, [do: body]]
-         },
-         acc
-       ) do
-    parse_body(body, acc)
-  end
-
-  defp _parse({:@, _, [{:safety_assured, _, [name]}]}, acc) do
-    [{:safety_assured, name} | acc]
-  end
-
-  defp _parse({_name, _meta, args}, acc) do
-    _parse(args, acc)
-  end
-
-  defp _parse({:do, tuple}, acc) do
-    _parse(tuple, acc)
-  end
-
-  defp _parse([head | tail], acc) do
-    new_acc = _parse(head, acc)
-    _parse(tail, new_acc)
-  end
-
-  defp _parse([], acc), do: acc
-
-  defp _parse(_other, acc), do: acc
-
-  defp parse_body(body, acc) do
-    dangers = check_for_execute(body) ++ check_for_index_concurrently(body)
-    dangers = Enum.reject(dangers, &is_nil/1)
-    acc ++ dangers
-  end
-
-  defp check_for_execute({:execute, location, _}) do
-    [{:raw_sql, Keyword.get(location, :line)}]
-  end
-
-  defp check_for_execute([head | tail]) do
-    check_for_execute(head) ++ check_for_execute(tail)
-  end
-
-  defp check_for_execute(_), do: []
-
-  defp check_for_index_concurrently(
+  defp find_index_not_concurrently(
          {:create, location, [{:index, _, [_table, _columns, options]}]}
        ) do
     case Keyword.get(options, :concurrently) do
@@ -72,9 +24,17 @@ defmodule ExcellentMigrations.Parser do
     end
   end
 
-  defp check_for_index_concurrently([head | tail]) do
-    check_for_index_concurrently(head) ++ check_for_index_concurrently(tail)
+  defp find_index_not_concurrently(_), do: []
+
+  defp find_raw_sql({:execute, location, _}) do
+    [{:raw_sql, Keyword.get(location, :line)}]
   end
 
-  defp check_for_index_concurrently(_), do: []
+  defp find_raw_sql(_), do: []
+
+  defp find_safety_assured({:@, _, [{:safety_assured, _, [value]}]}) do
+    [{:safety_assured, value}]
+  end
+
+  defp find_safety_assured(_), do: []
 end

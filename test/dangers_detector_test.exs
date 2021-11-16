18b83b92a1bc07e59b73cb3d754a75ee18e8d462
Artur Sulej <mr.artur.su@gmail.com>
2021-11-16 21:20:52 +0100

    Improved tests

---
+++ test/dangers_detector_test.exs
@@ -0,0 +1,24 @@
+defmodule ExcellentMigrations.DangersDetectorTest do
+  use ExUnit.Case
+  alias ExcellentMigrations.DangersDetector
+
+  test "detects dangers in AST" do
+    ast =
+      Code.string_to_quoted!(
+        File.read!("test/example_migrations/20191026103002_execute_raw_sql.exs")
+      )
+
+    assert [{:raw_sql_executed, 3}, {:raw_sql_executed, 7}] == DangersDetector.detect_dangers(ast)
+  end
+
+  test "skips dangers with safety assured" do
+    ast =
+      Code.string_to_quoted!(
+        File.read!(
+          "test/example_migrations/20191026103004_execute_raw_sql_with_safety_assured.exs"
+        )
+      )
+
+    assert [] == DangersDetector.detect_dangers(ast)
+  end
+end
--- test/runner_test.exs
+++ test/runner_test.exs
@@ -6,26 +6,56 @@ defmodule ExcellentMigrations.RunnerTest do
     file_paths = [
       "test/example_migrations/20191026103001_create_table_and_index.exs",
       "test/example_migrations/20191026103002_execute_raw_sql.exs",
-      "test/example_migrations/20191026103003_create_table.exs"
+      "test/example_migrations/20191026103003_create_table.exs",
+      "test/example_migrations/20191026103004_execute_raw_sql_with_safety_assured.exs",
+      "test/example_migrations/20191026103005_remove_column.exs",
+      "test/example_migrations/20191026103006_rename_table.exs",
+      "test/example_migrations/20191026103007_add_column_with_default_value.exs",
+      "test/example_migrations/20191026103008_change_column_type.exs"
     ]
 
     assert {
              :dangerous,
              [
                %{
-                 path: "test/example_migrations/20191026103001_create_table_and_index.exs",
                  line: 8,
+                 path: "test/example_migrations/20191026103001_create_table_and_index.exs",
                  type: :index_not_concurrently
                },
                %{
-                 path: "test/example_migrations/20191026103002_execute_raw_sql.exs",
                  line: 3,
+                 path: "test/example_migrations/20191026103002_execute_raw_sql.exs",
                  type: :raw_sql_executed
                },
                %{
-                 path: "test/example_migrations/20191026103002_execute_raw_sql.exs",
                  line: 7,
+                 path: "test/example_migrations/20191026103002_execute_raw_sql.exs",
                  type: :raw_sql_executed
+               },
+               %{
+                 line: 4,
+                 path: "test/example_migrations/20191026103005_remove_column.exs",
+                 type: :column_removed
+               },
+               %{
+                 line: 3,
+                 path: "test/example_migrations/20191026103006_rename_table.exs",
+                 type: :table_renamed
+               },
+               %{
+                 line: 4,
+                 path: "test/example_migrations/20191026103007_add_column_with_default_value.exs",
+                 type: :column_added_with_default
+               },
+               %{
+                 line: 5,
+                 path: "test/example_migrations/20191026103007_add_column_with_default_value.exs",
+                 type: :column_added_with_default
+               },
+               %{
+                 line: 4,
+                 path: "test/example_migrations/20191026103008_change_column_type.exs",
+                 type: :column_type_changed
                }
              ]
            } == Runner.check_migrations(migrations_paths: file_paths)

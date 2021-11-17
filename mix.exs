bd75314ed40e1c8354efdbffa00d7c9df2347050
Artur Sulej <mr.artur.su@gmail.com>
2021-11-17 22:11:36 +0100

    Elixir version >= 1.7.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExcellentMigrations.MixProject do
     [
       app: :excellent_migrations,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: ">= 1.7.0",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]

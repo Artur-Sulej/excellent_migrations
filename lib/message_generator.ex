f054253d2ed2ea49a09e33b5f54ef526addcc153
Artur Sulej <mr.artur.su@gmail.com>
2021-11-25 08:08:37 +0100

    Improved message_generator

--- lib/message_generator.ex
+++ lib/message_generator.ex
@@ -5,55 +5,10 @@ defmodule ExcellentMigrations.MessageGenerator do
     "#{build_message(type)} in #{path}:#{line}"
   end
 
-  def build_message(:raw_sql_executed) do
-    "Raw SQL used"
-  end
-
-  def build_message(:index_not_concurrently) do
-    "Index added not concurrently"
-  end
-
-  def build_message(:many_columns_index) do
-    "Index added many columns"
-  end
-
-  def build_message(:column_added_with_default) do
-    "Column with default value added to existing table"
-  end
-
-  def build_message(:column_removed) do
-    "Column removed"
-  end
-
-  def build_message(:table_dropped) do
-    "Table dropped"
-  end
-
-  def build_message(:table_renamed) do
-    "Table renamed"
-  end
-
-  def build_message(:column_renamed) do
-    "Column renamed"
-  end
-
-  def build_message(:not_null_added) do
-    "Not null constraint added"
-  end
-
-  def build_message(:column_type_changed) do
-    "Column type changed"
-  end
-
-  def build_message(:operation_insert) do
-    "Records inserted"
-  end
-
-  def build_message(:operation_update) do
-    "Records updated"
-  end
-
-  def build_message(:operation_delete) do
-    "Records deleted"
+  def build_message(type) do
+    type
+    |> Atom.to_string()
+    |> String.replace("_", " ")
+    |> String.capitalize()
   end
 end

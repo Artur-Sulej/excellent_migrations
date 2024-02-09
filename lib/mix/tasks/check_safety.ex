ef82db820cd53df512794d8d94688274a5648dab
Artur Sulej <artur@shedul.com>
2024-02-09 06:59:55 +0100

    Using Logger.warning instead of deprecated Logger.warn

--- lib/mix/tasks/check_safety.ex
+++ lib/mix/tasks/check_safety.ex
@@ -21,7 +21,7 @@ defmodule Mix.Tasks.ExcellentMigrations.CheckSafety do
         Enum.each(dangers, fn danger ->
           danger
           |> ExcellentMigrations.MessageGenerator.build_message()
-          |> Logger.warn()
+          |> Logger.warning()
         end)
 
         exit({:shutdown, 1})
